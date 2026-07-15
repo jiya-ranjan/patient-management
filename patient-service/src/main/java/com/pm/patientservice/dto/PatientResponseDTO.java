88ccf06a6f31b8e768017b5865d28acd9e23c936
Jiya Ranjan <ranjanjiya55@gmail.com>
2026-07-15 22:31:28 +0530

    Initial commit

---
+++ patient-service/src/main/java/com/pm/patientservice/dto/PatientResponseDTO.java
@@ -0,0 +1,50 @@
+package com.pm.patientservice.dto;
+
+public class PatientResponseDTO {
+    private String id;
+    private String name;
+    private String email;
+    private String address;
+    private String dateofBirth;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDateofBirth() {
+        return dateofBirth;
+    }
+
+    public void setDateofBirth(String dateofBirth) {
+        this.dateofBirth = dateofBirth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
